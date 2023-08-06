5b96bfbfdaa79aa1e4ff3e7c87a646a94a5ed42e
Rossita <47862499+AaronsonRossita@users.noreply.github.com>
2023-08-06 21:07:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,169 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = -5;
+        int b = 18;
+//
+//        int sum = a + b;
+//        System.out.println("sum = " + sum);
+//        int dif = a - b;
+//        System.out.println("dif = " + dif);
+//        int mult = a * b;
+//        System.out.println("mult = " + mult);
+//        int div = a / b;
+//        System.out.println("div = " + div);
+//
+//        a = 13;
+//        System.out.println("modulus = " + a % b);
+//
+//
+//        System.out.println("a++ = " + a++);
+//        System.out.println(a);
+//
+//        System.out.println("b-- = " + b--);
+//        System.out.println(b);
+//
+//        a = a + 7;
+//        a += 7;
+//
+//        System.out.println("after += " + a);
+//
+//        System.out.println("<= = " + (6 <= 6));
+//
+//        System.out.println("and = " + (a > 0 && a < 20));
+//        System.out.println("or = " + (a > 0 || a < 20));
+//
+//        boolean bool = true;
+//        System.out.println("boolean = " + !bool);
+//
+//    IF statements
+//        if (a > b){
+//            System.out.println("a is bigger");
+//        }
+//
+//        if(a > b){
+//            System.out.println("a is bigger");
+//        }else{
+//            System.out.println("a is not bigger");
+//        }
+//
+//        if(a > b){
+//            System.out.println("a is bigger");
+//        }else if (a < b){
+//            System.out.println("a is smaller");
+//        }else{
+//            System.out.println("a and b are equal");
+//        }
+//
+//        if (a == 18){
+//            System.out.println("a is 18");
+//        }
+//
+//        if(a > 4){
+//            System.out.println("a is bigger than 4 from else if");
+//        }
+//
+//        if(a > 0){
+//            if(a > 10){
+//                System.out.println("bigger than 10");
+//            }else{
+//                System.out.println("between 1 and 9");
+//            }
+//        }
+//
+//        if(a > 0){
+//            System.out.println("bigger than zero");
+//        }else{
+//            System.out.println("not bigger than zero");
+//        }
+//
+//        System.out.println(a > 0 ? "bigger than zero" : "not bigger than zero");
+//        int day = -3;
+//
+//        switch (day){
+//            case 1:
+//                System.out.println("Mon");
+//                break;
+//            case 2:
+//                System.out.println("Tue");
+//                break;
+//            case 3:
+//                System.out.println("Wed");
+//                break;
+//            case 4:
+//                System.out.println("Thu");
+//                break;
+//            case 5:
+//                System.out.println("Fri");
+//                break;
+//            case 6:
+//                System.out.println("Sat");
+//                break;
+//            case 7:
+//                System.out.println("Sun");
+//                break;
+//            default:
+//                System.out.println("not a week day number");
+//        }
+
+        //switch without break
+//
+//        switch (day){
+//            case 1:
+//                System.out.println("Mon");
+//            case 2:
+//                System.out.println("Tue");
+//            case 3:
+//                System.out.println("Wed");
+//            case 4:
+//                System.out.println("Thu");
+//            case 5:
+//                System.out.println("Fri");
+//            case 6:
+//                System.out.println("Sat");
+//            case 7:
+//                System.out.println("Sun");
+//            default:
+//                System.out.println("not a week day number");
+//
+//        }
+
+        String fruit = "Banana";
+
+        switch (fruit){
+            case "Banana":
+                System.out.println("Banana");
+                break;
+            case "Apple":
+                System.out.println("Apple");
+                break;
+            case "Pear":
+                System.out.println("Pear");
+                break;
+            case "Grapes":
+                System.out.println("Grapes");
+                break;
+            default:
+                System.out.println("sorry not in the fruit list");
+        }
+        System.out.println("after");
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
+
+
+
